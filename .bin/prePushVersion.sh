21fc4674e7619962b3515a660b5c0ef6cb91c121
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-04-21 17:32:51 -0500

    Add versioning

---
+++ .bin/prePushVersion.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# always patch the version number on update
+set -e
+
+[ -n "$_CUSTOM_RESOURCES_INNER_PRE_HOOK" ] && {
+  echo "\n ### version patch successful"
+  exit 0
+}
+
+echo "\n> pre-push: patching version number and resubmitting push"
+npm version patch --force
+_CUSTOM_RESOURCES_INNER_PRE_HOOK=1 git push --follow-tags
+echo
+echo " ### npm version bumped and pushed by git hook "
+echo " ### outer push will be canceled, ignore error below "
+echo " "
+exit 1
+
---
+++ .huskyrc.js
@@ -0,0 +1,33 @@
+const tasks = arr => arr.join(' && ');
+
+const hooks = {
+  // 'pre-commit': tasks(['npm run lint', 'npm run test']),
+  // 'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+  'pre-push': tasks([/* 'npm run test',  */ 'npm run todos', '.bin/prePushVersion.sh']),
+  'post-push': tasks(['git push --tags --no-verify', 'npm run lint'])
+};
+
+module.exports = { hooks };
+
+// applypatch-msg
+// commit-msg
+// husky.local.sh
+// husky.sh
+// post-applypatch
+// post-checkout
+// post-commit
+// post-merge
+// post-receive
+// post-rewrite
+// post-update
+// pre-applypatch
+// pre-auto-gc
+// pre-commit
+// pre-merge-commit
+// pre-push
+// pre-rebase
+// pre-receive
+// prepare-commit-msg
+// push-to-checkout
+// sendemail-validate
+// update
